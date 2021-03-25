808d9e4caff0453a8f16a10d837a62bb39e3bd4f
rmx911 <rohan.mujoo1@gmail.com>
2021-03-25 15:15:46 +0530

    assigment week 2

---
+++ AssigmentW2/assignmentw2/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
