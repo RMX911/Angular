10c26585e01ce62c938e96c7b384b1073b80dc8d
rmx911 <rohan.mujoo1@gmail.com>
2021-04-15 11:14:52 +0530

    created homepage/header/footer component

--- Edureka project/edurekaTicketBooking/src/app/app.module.ts
+++ Edureka project/edurekaTicketBooking/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+import { HomepageComponent } from './components/homepage/homepage.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomepageComponent 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
